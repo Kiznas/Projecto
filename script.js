e6280e775babbb405c6c06fa538f8920316633c3
Kinza <klimtverdov@gmail.com>
2020-04-29 19:12:32 +0300

    commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strcict';
+
+let a = 5;
+console.log(a);
+
+var string = 'hello';
+var boolean = true;
+null;
+var obj = {};
+
+
+
+
+console.log(4/0);
+console.log('string'*9);
+let person ={
+    name:"John",
+    age: 25,
+    isMArried: false
+};
+
+console.log(person["name"]);
+
+let photo = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(photo[0]);
+
+//alert("hello world");
+
+//let answer = confirm("Do u wanna play with me");
+//console.log(answer);
+
+//let answer = +prompt("18 есть ?");
+
+//console.log(typeof(answer));
+
+//console.log ("arr" + "-object");
+//console.log (4 + +"-object");
+
+let incr = 10,
+    dece= 10;
+
+
+console.log (incr++);
+console.log (dece--);
+
+console.log(5%2);
+console.log("2"=== 2);
+
